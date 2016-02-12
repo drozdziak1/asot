cdcc4a0e230defda8573bd635be4f72f626cb97f
Stanisław Drozd <drozdziak1@gmail.com>
2016-02-13 00:51:44 +0100

    Add insertionsort

--- insertionsort.c
+++ insertionsort.c
@@ -1,11 +1,16 @@
 #include <stdio.h>  // printf()
 #include <stdlib.h> // calloc(), atoi()
+#include <time.h>   // time()
 
 void insertionsort(int* nums, int count); // insertionsort an array of ints
+void swap(int* a, int* b);
 
 int main(int argc, char const* argv[])
 {
+    srand(time(NULL));
+
     int* nums = calloc(argc - 1, sizeof(int));
+
     for (int i = 0; i < argc - 1; i++) {
         nums[i] = atoi(argv[i + 1]);
     }
@@ -16,7 +21,7 @@ int main(int argc, char const* argv[])
 
     puts("\n");
 
-    insertionsort(nums, argc-1);
+    insertionsort(nums, argc - 1);
 
     puts("Your arguments (sorted):");
     for (int i = 0; i < argc - 1; i++)
@@ -28,21 +33,16 @@ int main(int argc, char const* argv[])
 
 void insertionsort(int* nums, int count)
 {
-    int swaps, temp;
+    // i - leftmost element of the unsorted part
+    for (int i = 1; i < count; i++)
+        // j - element currenly being sorted
+        for (int j = i; j > 0 && nums[j - 1] > nums[j]; j--)
+            swap(nums + j - 1, nums + j);
+}
 
-    do {
-        swaps = 0;
-
-        for (int i = 0; i < count-1; i++) {
-
-            if (nums[i] > nums[i+1])
-            {
-                temp = nums[i];
-                nums[i] = nums[i+1];
-                nums[i+1] = temp;
-
-                ++swaps;
-            }
-        }
-    } while (swaps);
+void swap(int* a, int* b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }

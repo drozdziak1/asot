f459327173be1d5160a70387d11fdf62f00bab9e
Stanisław Drozd <drozdziak1@gmail.com>
2016-02-15 01:17:19 +0100

    Add some comments to clarify radix

--- radixsort.c
+++ radixsort.c
@@ -8,7 +8,14 @@
 void radixsort(int* nums, int count); // Radixsort an array of ints (LSD variant)
 
 int radix_len(int num); // Returns decimal digit count of a number
-// Returns a digit on pos position, radix_digit(*something*, 1) returns the LSD
+
+/* This function returns a digit on pos position.
+ * radix_digit(*something*, 1) returns the LSD.
+ *
+ * NOTE: Bear in mind that it's capable of
+ * returning values below 0 for
+ * negative numbers.
+ */
 int radix_digit(int num, int pos);
 
 int main(int argc, char const* argv[])
@@ -37,7 +44,7 @@ int main(int argc, char const* argv[])
 void radixsort(int* nums, int count)
 {
     int digit, max = radix_len(nums[0]);
-    Queue buckets[19]; // One index per digit
+    Queue buckets[19]; // One index per digit, including negatives
 
     for (int i = 0; i < 19; i++) { // Nullify the shit outta each queue's empty head
         buckets[i].head = NULL;
@@ -48,23 +55,23 @@ void radixsort(int* nums, int count)
         if (radix_len(nums[i]) > max)
             max = radix_len(nums[i]);
 
+    // Go through each column
     for (int i = 1; i <= max; i++) {
+        // of each element
         for (int j = 0; j < count; j++) {
 
-            // Get the i-th digit from j-th nums[] element
-            digit = radix_digit(nums[j], i);
+            // to get the i-th digit from j-th nums[] element,
+            digit = radix_digit(nums[j], i) + 9; // compensate for negative digits with 9,
 
-            // Enqueue j-th element to the corresponding digit-th bucket
-            q_enq(buckets + digit + 9, nums[j]);
+            // and enqueue j-th element to the corresponding digit-th bucket.
+            q_enq(buckets + digit, nums[j]);
         }
 
-        for (int j = 0; j < count; j++) {
-            for (int k = 0; k < 19; k++) {
-                while (buckets[k].head != NULL) {
+        // Take each element of nums[],
+        for (int j = 0; j < count; j++)
+            for (int k = 0; k < 19; k++)        // and fill it with whatever
+                while (buckets[k].head != NULL) // we find in each non-empty bucket.
                     nums[j++] = q_deq(buckets + k);
-                }
-            }
-        }
     }
 }
 
@@ -81,9 +88,8 @@ int radix_len(int num)
 
 int radix_digit(int num, int pos)
 {
-    if (pos < 1) {
+    if (pos < 1)
         die("Requested a digit on an invalid position");
-    }
 
     num %= (int)pow(10, pos);     // Chop the left part off
     num /= (int)pow(10, pos - 1); // Chop the right part off
